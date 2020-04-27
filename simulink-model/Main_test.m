c96a057e68c69f6ea8503a78f9c857a21ed8133e
matjoh <matjoh98@gmail.com>
2020-04-27 09:06:13 +0200

    Project 3 Final.
    Pretty good tuning for the LQ. Could be tuned better though.
    
    Ready for evaluation section in the report.

--- simulink-model/Main_test.m
+++ simulink-model/Main_test.m
@@ -1,41 +1,19 @@
 clear all, clc
 load('FlightData.mat')
-% D:\project1_git\project2020-group1\simulink-model
-
-
-% Complementary filter coefficents
-alpha = 0.1;
-Ts = 0.01;
-
-% Controller gain
-P_gain = 40000;
-
-%%
-% Angles for acceleration
-% phi_a = atan2(fby,fbz);
-% theta_a = atan2(-fbx,sqrt((fby)^2+(fbz)^2));
-
-% Rotation matrices 
-syms phi theta psi dphi dtheta dpsi
-rot_x = [1 0 0;0 cos(phi) -sin(phi);0 sin(phi) cos(phi)]
-rot_y = [cos(theta) 0 sin(theta);0 1 0; -sin(theta) 0 cos(theta)]
-rot_z = [cos(psi) -sin(psi) 0; sin(psi) cos(psi) 0; 0 0 1]
-
-R=rot_z*rot_y*rot_x
-
-
-w = [dphi;0;0]+rot_x*[0;dtheta;0]+rot_x*rot_y*[0;0;dpsi]
 
 %% LQ controller
 clear all, clc, close all
+cd D:\project1_git\project2020-group1\simulink-model
 load('FlightData.mat')
 
 % Complementary filter coefficents
 alpha = 0.1;
 Ts = 0.01;
 g = 9.81;
+m = 0.027;
 Ts = 0.01;
 
+base_thrust = ((m*g)/4)*(1/(0.06/4*g/65536)); % Base thrust
 
 A = [0     0     0     1     0     0;
      0     0     0     0     1     0;
@@ -53,20 +31,19 @@ A = [0     0     0     1     0     0;
 
 C = [1 0 0 0 0 0;
      0 1 0 0 0 0]; 
- 
 D = zeros(2,4);
 
 Ad = expm(A*Ts);
-Bd = (pinv(A)*expm(A*Ts)*B-pinv(A)*B); % Add this: *0.06/4*g/65536 : And remove 
+Bd = (pinv(A)*expm(A*Ts)*B-pinv(A)*B)*0.06/4*g/65536;
 
 Cdnew = [1 0 0 0 0 0;
          0 1 0 0 0 0];
 
-Qx = 10*diag([1 1 1 1 1 1]);    % Tune these
-Qu = 10*diag([1 1 1 1]);        % Tune these
+% Qu = diag([100 1 100 1]);        % Tune these
+Qx = diag([1000 1000 1 10 10 10])*10;    % Tune these
+Qu = 0.5;        % Tune these
 
-Klqr = dlqr(Ad,Bd,Qx,Qu)
-
-Klqr = [Klqr(:,1:2) Klqr(:,4:end)]
+Klqr = dlqr(Ad,Bd,Qx,Qu);   
+Klqr = [Klqr(:,1:2) Klqr(:,4:end)] % Only use phi, theta, wx, wy and wz.
 
 'done'
